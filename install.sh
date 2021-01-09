c48a47ed60bce471263630a5c8b6ee863be4b9fa
Mazhar Hussain <mazhar@mazhar-pc.mazhar-pc.com>
2021-01-09 14:22:03 +0500

    Added dependency check in install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,17 @@
 currentDir=$(realpath $(dirname $0))
 binDir="$root"/usr/local/bin
 manDir="$root"/usr/local/man
+
+if ! which glib-compile-resources gresource > /dev/null; then
+  if which apt > /dev/null; then
+    sudo apt install libglib2.0-dev
+  else
+    echo "This script needs GLib (developer edition) in order to work. But it was not found." > /dev/stderr
+    echo "Its package name may be 'glib2', 'glib2-devel', 'libglib2.0-dev', or straight up 'glib', etc. depending on your distro."
+    exit 1
+  fi
+fi
+
 if [ $UID = '0' ]; then
   echo 'installing gdm-tools ...'
   mkdir -p "$binDir" "$manDir"/man1
