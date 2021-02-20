a7ee24e02724e68283ccb90b5bb90cf5f638ca13
Mazhar Hussain <realmazharhussain@gmail.com>
2021-02-20 11:33:54 +0500

    Better Installation Script

--- install.sh
+++ install.sh
@@ -5,12 +5,20 @@ manDir="$root"/usr/local/man
 fishComp="$root"/usr/local/share/fish/vendor_completions.d
 confDir="$root"/etc/gdm-tools
 
-if ! which glib-compile-resources gresource > /dev/null; then
-  if ! ( which apt &> /dev/null && sudo apt install libglib2.0-dev ); then
+depMsg() {
     echo "This script needs GLib (developer edition) in order to work. But it is not installed." > /dev/stderr
     echo "Please, install it first in order to install gdm-tools"
     echo "Its package name may be 'glib2', 'glib2-devel', 'libglib2.0-dev', or straight up 'glib', etc. depending on your distro."
     exit 1
+}
+
+if ! which glib-compile-resources gresource &> /dev/null; then
+  if which apt &> /dev/null; then
+    sudo apt install libglib2.0-dev || depMsg
+  elif which pacman &> /dev/null; then
+    sudo pacman -S glib2 || depMsg
+  else
+    depMsg
   fi
 fi
 
